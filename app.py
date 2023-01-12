eb93607040dec49b22e1c7c23d47d3297c5fefb6
Janengethe <janengethew@gmail.com>
2023-01-12 15:46:14 +0300

    registration

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import About
+from models import About, User
 
 @app.route("/")
 def hello():
@@ -24,6 +24,34 @@ def get_details():
     name=request.args.get('name')
     about=request.args.get('about')
     return "Name : {}, About: {}".format(name,about)
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         password = request.form['password']
+#         role = request.form['role']
+#         new_user = User(name=name, email=email, password=password, role=role)
+#         db.session.add(new_user)
+#         db.session.commit()
+#         flash('You are registered!')
+#         return redirect(url_for('login'))
+#     return render_template('register.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        role = request.form['role']
+
+        new_user = User(name=name, email=email, password=password, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({'message': 'New user created successfully.'}), 201
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import db
+from sqlalchemy.dialects.postgresql import ENUM
 
 class About(db.Model):
     __tablename__ = 'about'
@@ -20,3 +21,26 @@ class About(db.Model):
             'name': self.name,
             'about':self.about
         }
+
+class User(db.Model):
+    role_enum = ENUM('owner', 'member', name='role_enum')
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    role = db.Column(role_enum, nullable=False)
+
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     email = db.Column(db.String(255), unique=True)
+#     password = db.Column(db.String(255))
+#     role = db.Column(db.Enum('owner', 'member', name='role_enum'))
+#     member = db.relationship("Member", uselist=False, back_populates="user")
+    
+# class Member(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(255))
+#     date_joined = db.Column(db.DateTime, default=datetime.utcnow)
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     user = db.relationship("User", back_populates="member")
