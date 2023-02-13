e3a54065b8fd51a015fd9055d22262ddbfeed2d3
Janengethe <janengethew@gmail.com>
2023-02-13 13:59:43 +0300

    db

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
-    SQLALCHEMY_DATABASE_URI = os.environ['postgres://akldbagkltqwej:9eeffab6ddd07e05fed9df47a2c61e7aeed4e6eadd76571aff26796dfe78f241@ec2-3-209-124-113.compute-1.amazonaws.com:5432/d7o8p9tmsr6jqo']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
     DEBUG = False
